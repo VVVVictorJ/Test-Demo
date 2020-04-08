deb813eff5995c2b05295c19519d8207f9f2be57
VVVVictorJ <956951190@qq.com>
2020-04-08 11:20:54 +0800

    巧妙解决退出登录

--- app/src/main/java/com/victor/test_demo/UI/Second/UserProfileFragment.java
+++ app/src/main/java/com/victor/test_demo/UI/Second/UserProfileFragment.java
@@ -1,5 +1,6 @@
 package com.victor.test_demo.UI.Second;
 
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -9,6 +10,10 @@ import com.bumptech.glide.Glide;
 import com.victor.test_demo.R;
 import com.victor.test_demo.UI.BaseFragment;
 import com.victor.test_demo.aspectj.annotation.LoginFilter;
+import com.victor.test_demo.utils.SharePreference.SharePreferenceUtil;
+import com.victor.test_demo.utils.modules.LoginActivity;
+
+import java.util.logging.Handler;
 
 public class UserProfileFragment extends BaseFragment {
     @Override
@@ -26,7 +31,10 @@ public class UserProfileFragment extends BaseFragment {
             @LoginFilter()
             @Override
             public void onClick(View view) {
-
+                SharePreferenceUtil.setBooleanSp(SharePreferenceUtil.IS_LOGIN,
+                        false, getActivity());
+                SharePreferenceUtil.setUser(SharePreferenceUtil.USER_NAME,
+                        "",getActivity());
             }
         });
         Glide.with(this).load("http://192.168.3.107/image/1.png").fitCenter().into(imageView);
